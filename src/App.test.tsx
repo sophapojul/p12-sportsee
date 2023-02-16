78c044b35303e407bda45356bedbbca93fa28d28
Pascal Allau <pascal.allau@zohomail.eu>
2023-02-16 21:23:17 +0100

    chore: initial commit and clean up app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hi', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/hi/i);
+  expect(element).toBeInTheDocument();
 });
